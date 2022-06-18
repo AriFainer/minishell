c7e9af349fac71d1b6dfee6142171d3334ae96f2
Martin Wildbaum <martinwildbaum01@gmail.com>
2022-06-18 19:53:13 -0300

    fhchg

--- builtin_getenv.c
+++ builtin_getenv.c
@@ -6,6 +6,12 @@
 int
 builtin_getenv(int argc, char **argv)
 {
+    if (argc < 2) {
+        fprintf(stderr, "Error: Sintaxis incorrecta del comando \"getenv\"\n");
+        char *help_argv[] = {"help", "getenv"};
+        builtin_help(2, help_argv);
+        return EXIT_FAILURE;
+    }
     char *env;
     for (int i = 1; i < argc; i++){
         if ((env = getenv(argv[i])) != NULL)
